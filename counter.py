f360d6d119f6a047fb541079b42f8557a439635a
Dean Dussaq <Zdussaq@gmail.com>
2020-11-10 23:07:48 -0800

    Outputting more specific data - going to look into better sorting and counting tomorrow - need to research data frames

--- counter.py
+++ counter.py
@@ -7,8 +7,15 @@ if __name__ == "__main__":
 	# create Spark context with necessary configuration
 	sc = SparkContext("local","PySpark Word Count Exmaple")
 	
+	dataFile = open("C:\\Users\\Zduss\\Spark\\learning\\data\\0.txt")
+	wordList = []
+	for line in dataFile:
+		content = line.split('\t')
+		for i in range(9, len(content)):
+			wordList.append(content[i].rstrip())
+
 	# read data from text file and split each line into words
-	words = sc.textFile("C:\\Users\\Zduss\\Spark\\learning\\data\\4.txt").flatMap(lambda line: line.split("\t"))
+	words = sc.parallelize(wordList)
 	
 	# count the occurrence of each word
 	wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
